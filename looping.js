23334146c0f716e1137c3ac013514a84862d7b70
saravananb15 <63905051+saravananb15@users.noreply.github.com>
2021-07-17 11:44:06 +0530

    Create looping.js

---
+++ looping.js
@@ -0,0 +1,83 @@
+// // // //If else
+// // // if(condition){
+// // //     statement
+// // // }else if(anotherCondition){
+// // //     statement
+// // // }else{
+// // //     statement
+// // // }
+
+// // //hour is between 6am to 12pm: Good morning!
+// // //hour is between 12pm to 6pm: Good Afternoon!
+// // // otherwise: Good evening
+
+// // let hour=10;
+// // if(hour>6 && hour <12){
+// //     console.log('Good Morning');
+// // }else if(hour > 12 && hour<= 18){
+// //     console.log('Good AfterNoon');
+// // }else{
+// //     console.log('Good Evening');
+// // }
+
+
+
+// // Switch Case
+// let role;
+// switch (role){
+//     case 'guest':
+//         console.log('Guest User');
+//         break;
+//     case 'Moderator':
+//         console.log('Moderator User');
+//         break;
+//     default:
+//         console.log('Unknown User');
+// }
+
+// //Easy way then siwtch
+// if(role === 'guest') console.log('Guest');
+// else if(role === 'moderator') console.log('moderator');
+// else console.log('Unknown User');
+
+//For loop
+// for(let i=0;i<5;i++){
+//     console.log('Hello World',i);
+// }
+
+//while 
+// let i=1;
+// while(i<=5){
+//     console.log('Hello world',i);
+//     i++;
+// }
+
+//do while loop
+// let i=0;
+// do{
+//     console.log('Hello World',i);
+//     i++;
+// }while(i<5);
+
+// Infinite loops
+// let i=0;
+// while(i<5){
+//     console.log('Hello World',i);
+// }
+
+//for in
+// const person={
+//     name:'Mosh',
+//     age: 30
+// };
+// for(let key in person)
+// console.log(key, person[key]);
+
+// const colors = ['red','green','blue'];
+// for(let index in colors)
+// console.log(index,colors[index]);
+
+//for of loop
+// const colors = ['red','green','blue'];
+// for(let color in colors)
+// console.log(color);
