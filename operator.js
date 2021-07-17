1e29ee67bc2e26579a09da99b47844518ac02985
saravananb15 <63905051+saravananb15@users.noreply.github.com>
2021-07-17 10:09:48 +0530

    Create operator.js

---
+++ operator.js
@@ -0,0 +1,77 @@
+// //arithmatic operators ( + , - , * , / , % )
+// // let x=10;
+// // let y=3;
+
+// // console.log(x+y);
+// // console.log(x-y);
+// // console.log(x*y);
+// // console.log(x/y);
+// // console.log(x%y);
+// // console.log(x**y);
+
+// // //Increament (++) 
+// // console.log(x);
+// // console.log(++x);//add 1 to x first and then print x
+
+// // console.log(x++);//print x first and then add 1 to x
+// // console.log(x);//to print the increamented value after doing (x++)
+
+// // //Increament (--) 
+// // console.log(--x);//decrement 1 to x first and then print x
+
+// // console.log(x--);//print x first and then add 1 to x
+// // console.log(x);//to print the increamented value after doing (x++)
+
+
+// // //Assignment operator (=)
+// // let x=10;
+// // //example
+// // x++;
+// // x=x+1;// equal to operator
+
+// // //comparion operator (!=,<,>,<=,>=,==)
+// // //relational operator
+// // console.log(1>0);
+// // console.log(1>=1);
+// // console.log(1<1);
+// // console.log(1<=1);
+
+// // //equality operator
+// // console.log(1==1);// same value
+// // console.log(1!=1);
+// // console.log(1===1);// same type and value
+// // console.log(1!==1);
+
+
+// // //Ternary Operator
+
+// // let points = 99;
+// // let type = points > 100 ? 'gold' : 'silver';
+// // console.log(type);
+
+// // //logical operands
+// // // logical and(&&) operator returns false if the one is false
+// // console.log(true && true);
+// // console.log(false&&false);
+// // console.log(true&&false);
+// // console.log(false&&true);
+// // //example for and operator
+// // let highIncome=true;
+// // let creditScore=true;
+// // let eligible = highIncome&&creditScore;
+// // console.log(eligible);
+
+// // // logical or (||) operator returns true if the one is true
+// // console.log(true || true);
+// // console.log(false||false);
+// // console.log(true||false);
+// // console.log(false||true);
+// // //example for and operator
+// let highIncome=false;
+// let creditScore=false;
+// let eligible = highIncome||creditScore;
+// console.log(eligible);
+
+// // Not operator(!)
+// let applicationRefused = !eligible;
+// console.log(applicationRefused);
