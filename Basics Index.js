5d0168252d030de015b3a471c945a4c42172acf9
saravananb15 <63905051+saravananb15@users.noreply.github.com>
2021-07-17 08:05:35 +0530

    Create Basics Index.js

---
+++ Basics Index.js
@@ -0,0 +1,35 @@
+
+// Introduction 
+//this is my first code
+// console.log("Hello World");
+
+// //Basics  impl
+// let name='sarvan';
+// console.log(name);
+
+// //2 var declaration check
+// let firstName="sarvan";
+// let lastName="B";
+// console.log(firstName,lastName);
+// console.log(firstName);
+// console.log(lastName);
+
+// //var(let) can be changed 
+// let interetRate = 0.3;
+// interetRate=1;
+// console.log(interetRate);
+
+// //constant cannot be reassigned.
+// const interestRate= 0.3;
+// // interestRate=1;
+// console.log(interestRate);
+
+//Primitive data types
+let name='Sarvan'; //String literal
+let age= 30; // Number literal
+let isApproved = true; //Boolean Literal
+let firstName; // undefined
+let firstNameExplicit= undefined; // explicitly wanted to make undefined
+let lastName= null; // null (ex: if user wants nothing to be changed).
+
+// JavaScript is dynamic Typing( difference between static and dynamic. Static cannot be changed the variable after declaration. Dynamic variable can be changed after declaration)
